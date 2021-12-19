d9b2c43e198b34a57bd070aceb22a4b350aa1436
ilya07092000 <ischenko2012@gmail.com>
2021-12-19 21:16:45 +0200

    feat: create cells

--- src/js/gameField.ts
+++ src/js/gameField.ts
@@ -1,9 +1,10 @@
 import { ITile } from './tile/tile';
-import { ICell } from './cell';
+import Cell, { ICell } from './cell';
 
 export interface IGameField {
   tiles: ITile[];
   rowSize: number;
+  createCells(): void;
 }
 
 class GameField implements IGameField {
@@ -32,6 +33,19 @@ class GameField implements IGameField {
 
   get rowSize(): number {
     return this._rowSize;
+  }
+
+  createCells() {
+    for (let i = 0; i < this._rowSize; i += 1) {
+      const row = [];
+
+      for (let j = 0; j < this._rowSize; j += 1) {
+        const cell: ICell = new Cell(j, i);
+        row.push(cell);
+      }
+
+      this._cells.push(row);
+    }
   }
 }
 
